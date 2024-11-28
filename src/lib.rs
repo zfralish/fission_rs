fe1bda1de53663f0c72fb5709a18f5ca29de31c8
zfralish <zekefralish@gmail.com>
2024-11-27 23:04:53 -0600

    setting up some structure for heat exchanger

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+pub mod config;
+pub mod models;
+pub mod physics;
+pub mod ui;
+pub mod utils;
---
+++ src/models/heat_exchanger/mod.rs
@@ -0,0 +1,4 @@
+pub mod fluids;
+pub mod geometry;
+pub mod heat_exchanger;
+pub mod operating_conditions;
